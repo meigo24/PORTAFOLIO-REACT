39960f35e369aed00efde90c0a8836154727f865
meigo24 <you@example.com>
2024-06-12 01:09:51 -0600

    segundo commit

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,13 +1,40 @@
 import React from "react";
+import './styles.css';
+
+
 
 function Inicio() {
   return(
-    <>
-    <h1>Leimy Páez Hurtado</h1>
-    <h2>Bienvenidos a mi portafolio</h2>
-    
-    
-    </>
-  )  
+<>
+<h1>Leimy Páez Hurtado</h1>
+<h2>Frontend</h2> <br></br>
+<br></br>
+<p className ="w -[600px] h-[400px]">
+   Mi nombre es Leimy Páez Hurtado,tengo 23 años,
+  <br></br>
+    naci y creci en Puerto Viejo, una pequeña region
+  <br></br>
+   al sur de la provincia de Limón,rodeada de mar y bosque natural.
+<br></br>
+   Crecí en un hogar con apoyo incodicial por parte de 
+<br></br>
+  mis padres ambos profesionales,y de nacionalidad Costarricense.
+<br></br>
+  Estudie una parte de mi vida en escuela y colegio publico 
+<br></br>
+ excepto mi ultimo año de bachillerato lo cual lo culmine
+<br></br>
+ en el instituto Centroamericano Adventista en el año 2018.
+<br></br>
+ Además ,me considero una persona con valores como la honestidad,
+ <br></br>
+  la empatia, la perseverancia y sobre todo la fe en Dios lo cual me motiva en mi día a día.
+
+</p>
+</>
+) 
 }
 export default Inicio
+
+
+  
