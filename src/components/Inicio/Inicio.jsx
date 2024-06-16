d6af759a7a1d7382fa436be20ba5b1af638f8150
meigo24 <109485821+meigo24@users.noreply.github.com>
2024-06-16 12:05:22 -0600

    actualizacion 16 de junio

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,40 +1,32 @@
 import React from "react";
-import './styles.css';
-
-
+import "./styles.css";
 
 function Inicio() {
-  return(
-<>
-<h1>Leimy Páez Hurtado</h1>
-<h2>Frontend</h2> <br></br>
-<br></br>
-<p className ="w -[600px] h-[400px]">
-   Mi nombre es Leimy Páez Hurtado,tengo 23 años,
-  <br></br>
-    naci y creci en Puerto Viejo, una pequeña region
-  <br></br>
-   al sur de la provincia de Limón,rodeada de mar y bosque natural.
-<br></br>
-   Crecí en un hogar con apoyo incodicial por parte de 
-<br></br>
-  mis padres ambos profesionales,y de nacionalidad Costarricense.
-<br></br>
-  Estudie una parte de mi vida en escuela y colegio publico 
-<br></br>
- excepto mi ultimo año de bachillerato lo cual lo culmine
-<br></br>
- en el instituto Centroamericano Adventista en el año 2018.
-<br></br>
- Además ,me considero una persona con valores como la honestidad,
- <br></br>
-  la empatia, la perseverancia y sobre todo la fe en Dios lo cual me motiva en mi día a día.
+  return (
+    <>
+      <div className="contenedor-perfil">
+        <h1>Leimy Páez Hurtado</h1>
+        <h2>Frontend Developer</h2>
+      
+    <img src="imagen.jpeg" width="250" ></img>
 
-</p>
-</>
-) 
+        <p className="parrafo-perfil">
+          Mi nombre es Leimy Páez Hurtado, tengo 23 años y nací y crecí en Puerto
+          Viejo, una pequeña región al sur de la provincia de Limón, rodeada de mar
+          y bosque natural. Crecí en un hogar con apoyo incondicional por parte
+          de mis padres, ambos profesionales, y de nacionalidad costarricense.
+        </p>
+
+        <p className="parrafo-perfil">
+          Estudié una parte de mi vida en escuela y colegio público, excepto mi
+          último año de bachillerato, el cual lo culminé en el Instituto
+          Centroamericano Adventista en el año 2018. Además, me considero una
+          persona con valores como la honestidad, la empatía, la perseverancia ySS
+          sobre todo la fe en Dios, lo cual me motiva en mi día a día.
+        </p>
+      </div>
+    </>
+  );
 }
-export default Inicio
 
-
-  
+export default Inicio;
